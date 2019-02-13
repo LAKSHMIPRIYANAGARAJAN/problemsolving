84beab2c40600e0f5ac3d3b6c201dfd85e66efce
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-02-13 15:04:26 +0530

    Update leapyr.py

--- leapyr.py
+++ leapyr.py
@@ -1,5 +1,11 @@
-n=int(input())
-if n%4==0:
-   print("yes")
+yr=int(input())
+if yr%4==0:
+   if yr%100==0:
+      if yr%400==0:
+         print("yes")
+      else:
+         print("no")
+   else:
+      print("yes")
 else:
    print("no")
